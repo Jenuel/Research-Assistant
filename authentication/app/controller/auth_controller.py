110f51181778f3e74a04c150058e0b9de6b3a16a
Renuel Balogo <renueljeremibalogo@gmail.com>
2025-04-06 12:37:19 +0800

    implement user registration functionality in auth_controller

--- authentication/app/controller/auth_controller.py
+++ authentication/app/controller/auth_controller.py
@@ -13,8 +13,27 @@ def authenticate_user():
     if not user or not user.check_password(data['password']):   
         return jsonify({"message": "Invalid username or password"}), 404
     
+def register_user():
+    data = request.get_json()
 
+    if data is None:
+        return jsonify({"message": "Missing data"}), 400
+    
+    existing_user = User.query.filter_by(email=data['email']).first()
 
+    if existing_user:
+        return jsonify({"message": "An existing user is already associated with this email"}), 409
+    
+    new_user = User(
+        email=data['email'],
+        password=data['password'],
+        first_name=data['first_name'],
+        last_name=data['last_name'],
+        created_at=db.func.now(),
+        updated_at=db.func.now()
+    )
+    
+    db.session.add(new_user)
+    db.session.commit()
 
-
-#def register_user():
+    return jsonify({"message": "User registered successfully"}), 201
