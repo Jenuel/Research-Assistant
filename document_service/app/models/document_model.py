ff2b27e87c0a11a25e7b5901a6eaaf38ea0e02c5
Renuel Balogo <renueljeremibalogo@gmail.com>
2025-06-04 15:50:08 +0800

    Update the Document model to fit the reading of the file

--- document_service/app/models/document_model.py
+++ document_service/app/models/document_model.py
@@ -1,5 +1,5 @@
 from app.db.database import Base
-from sqlalchemy import Column, Integer, String, DateTime, LargeBinary
+from sqlalchemy import Column, Integer, String, DateTime, Text
 from datetime import datetime
 
 class Document(Base):
@@ -8,7 +8,7 @@ class Document(Base):
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String, index=True)
     content_type = Column(String, index=True)
-    data = Column(LargeBinary)
+    data = Column(Text)
     created_at = Column(DateTime, default=datetime.utcnow)
     updated_at = Column(DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
 
