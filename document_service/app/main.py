8502c2d4e9e28bd8929e370df7cd985e73e8e5bc
Renuel Balogo <renueljeremibalogo@gmail.com>
2025-06-05 16:05:53 +0800

    bug fix

--- document_service/app/main.py
+++ document_service/app/main.py
@@ -5,6 +5,6 @@ app = FastAPI(title="FastAPI Backend for File Handling", version="0.1.0")
 
 app.include_router(document_routes.router, prefix="/api/documents", tags=["documents"])
 
-app.get("/")
+@app.get("/")
 def read_root():
     return {"message": "Welcome to the FastAPI backend for file handling!"}
