9bf577b4128f88fa32c6d0635b8b993463c94b7e
Renuel Balogo <renueljeremibalogo@gmail.com>
2025-06-07 15:28:52 +0800

    Switch from PersistentClient to HttpClient for chromadb connection

--- document_service/app/db/chroma_client.py
+++ document_service/app/db/chroma_client.py
@@ -1,4 +1,5 @@
 import chromadb
 
-chroma_client = chromadb.PersistentClient(path="./chroma_db")
+chroma_client = chromadb.HttpClient(host="localhost", port=8000)
+
 collection = chroma_client.get_or_create_collection(name="documents")
