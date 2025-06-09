6888794b33fe379b9f444ab1c18b6088243c173c
Renuel Balogo <renueljeremibalogo@gmail.com>
2025-06-09 18:27:24 +0800

    Integrated the chromadb server into the generation service and changed the port to 8080

--- document_service/app/db/chroma_client.py
+++ document_service/app/db/chroma_client.py
@@ -1,5 +1,5 @@
 import chromadb
 
-chroma_client = chromadb.HttpClient(host="localhost", port=8000)
+chroma_client = chromadb.HttpClient(host="localhost", port=8080)
 
 collection = chroma_client.get_or_create_collection(name="documents")
