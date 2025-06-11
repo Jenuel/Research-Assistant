45ce77daad579a77663eec1032c6b6c8f8baedc1
Renuel Balogo <renueljeremibalogo@gmail.com>
2025-06-11 13:01:11 +0800

    Changed structure to using env file

--- authentication/app/__init__.py
+++ authentication/app/__init__.py
@@ -12,10 +12,7 @@ def create_app():
 
     app = Flask(__name__, instance_relative_config=True)
 
-    app.config['SQLALCHEMY_DATABASE_URI'] = (
-        f"mysql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}"
-        f"@{os.getenv('DB_HOST')}/{os.getenv('DB_NAME')}"
-    )
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     db.init_app(app)
