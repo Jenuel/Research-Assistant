fba0b11d3e9f47ba56a77b3bd82d67949874d4d8
Renuel Balogo <renueljeremibalogo@gmail.com>
2025-06-12 15:38:25 +0800

    Refactor authentication methods to use Pydantic schemas for input validation

--- authentication/app/controller/auth_controller.py
+++ authentication/app/controller/auth_controller.py
@@ -1,39 +1,42 @@
-from app.models.user import User
-from app.db import db
+from ..models.user import User
+from ..db import db
+from ..schemas.user_schema import UserLoginSchema, UserRegistrationSchema
 from flask import request, jsonify
+from pydantic import ValidationError
 
 def authenticate_user():
-    data = request.get_json()
-
-    if not data or data.get('email') is None or data.get('password') is None:
-        return jsonify({"message": "Missing email or password"}), 400
+    try:
+        data = UserLoginSchema(**request.get_json())
+    except ValidationError as e:
+        return jsonify({"message": "Invalid input", "errors": e.errors()}), 400
     
-    user = User.query.filter_by(email=data['email']).first()
+    user = User.query.filter_by(email=data.email).first()
 
-    if not user or not user.check_password(data['password']):   
-        return jsonify({"message": "Invalid email or password"}), 404
+    if not user or not user.check_password(data.password):   
+        return jsonify({"message": "Invalid email or password"}), 401
     
     token = user.generate_token()
 
     return jsonify({"token": token}), 200
 
-def register_user():
-    data = request.get_json()
 
-    if data is None:
-        return jsonify({"message": "Missing data"}), 400
+def register_user():
+    try:
+        data = UserRegistrationSchema(**request.get_json())
+    except ValidationError as e:
+        return jsonify({"message": "Invalid input", "errors": e.errors()}), 400
     
-    existing_user = User.query.filter_by(email=data['email']).first()
+    existing_user = User.query.filter_by(email=data.email).first()
 
     if existing_user:
         return jsonify({"message": "An existing user is already associated with this email"}), 409
     
     new_user = User(
-        email=data['email'],
-        first_name=data['first_name'],
-        last_name=data['last_name'],
+        email=data.email,
+        first_name=data.first_name,
+        last_name=data.last_name,
     )
-    new_user.set_password(data['password'])
+    new_user.set_password(data.password)
     
     db.session.add(new_user)
     db.session.commit()
