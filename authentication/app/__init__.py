1b6f30bc8134adab50a69bf0389934cd37069611
Renuel Balogo <renueljeremibalogo@gmail.com>
2025-06-12 15:41:37 +0800

    Add exception handling in database

--- authentication/app/__init__.py
+++ authentication/app/__init__.py
@@ -1,8 +1,10 @@
-from flask import Flask
+from flask import Flask, jsonify
 from dotenv import load_dotenv
 import os
-from app.db import db
-from app.routes import auth_bp
+from .db import db
+from .routes.auth_routes import auth_bp
+from sqlalchemy import text
+from sqlalchemy.exc import OperationalError
 
 def create_app():
     """
@@ -16,6 +18,19 @@ def create_app():
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     db.init_app(app)
+
+    @app.route('/')
+    def index():
+        try:
+            db.session.execute(text('SELECT 1'))
+            db_status = "connected"
+        except OperationalError:
+            db_status = "not connected"
+
+        return jsonify({
+            "message": "Welcome to the Flask App!",
+            "database": db_status
+        })
 
     # Register blueprints
     app.register_blueprint(auth_bp, url_prefix='/auth')
