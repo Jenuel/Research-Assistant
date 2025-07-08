8dd1de09276e0169c66fb29b3faceff03317893f
Renuel Balogo <renueljeremibalogo@gmail.com>
2025-07-08 22:39:11 +0800

    Add database initialization on startup event

--- document_service/app/main.py
+++ document_service/app/main.py
@@ -1,9 +1,15 @@
 from fastapi import FastAPI
 from app.routes import document_routes
+from app.db.database import Base, engine
+from app.models.document_model import Document
 
 app = FastAPI(title="FastAPI Backend for File Handling", version="0.1.0")
 
 app.include_router(document_routes.router, prefix="/api/documents", tags=["documents"])
+
+@app.on_event("startup")
+def init_db():
+    Base.metadata.create_all(bind=engine)
 
 @app.get("/")
 def read_root():
