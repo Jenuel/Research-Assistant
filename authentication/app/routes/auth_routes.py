cf2dd0f407794edc6e0b13b6cccf0ac8d53bf1a9
Renuel Balogo <renueljeremibalogo@gmail.com>
2025-07-10 20:19:56 +0800

    Add logout route to authentication blueprint

--- authentication/app/routes/auth_routes.py
+++ authentication/app/routes/auth_routes.py
@@ -1,5 +1,5 @@
 from flask import Blueprint
-from ..controller.auth_controller import authenticate_user, register_user
+from ..controller.auth_controller import authenticate_user, register_user, logout_user
 
 auth_bp = Blueprint('auth', __name__)
 
@@ -10,3 +10,7 @@ def login():
 @auth_bp.route('/register', methods=['POST'])
 def register():
     return register_user()
+
+@auth_bp.route('/logout', methods=['POST'])
+def logout():
+    return logout_user
